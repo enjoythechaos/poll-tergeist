dd54f8ba3f135ddffec3bedc16bbbd35e9690a75
App Academy Student <student@appacademy.io>
2015-12-17 09:56:28 -0800

    This time without qs

--- frontend/polltergeist.jsx
+++ frontend/polltergeist.jsx
@@ -6,20 +6,20 @@ var Route = RouterBase.Route;
 var IndexRoute = RouterBase.IndexRoute;
 var Router = RouterBase.Router;
 
-//var PollStore = require('./stores/poll');
+var PollStore = require('./stores/poll');
 
-//var PollIndexPage = require('./components/poll_index_page');
-//var MultiPollForm = require('./components/multi_poll_form');
-//var PollEdit = require('./components/poll_edit');
+var PollIndexPage = require('./components/poll_index_page');
+var MultiPollForm = require('./components/multi_poll_form');
+var PollEdit = require('./components/poll_edit');
 
-// var routes = (
-//   <Route path="/users/:userId/polls">
-//     <IndexRoute component={PollIndexPage}/>
-//     <Route path="/users/:userId/polls/new" component={MultiPollForm}></Route>
-//     <Route path="/users/:userId/polls/:pollId/edit" component={PollEdit}></Route>
-//   </Route>
-// );
-//
-// document.addEventListener('DOMContentLoaded', function(){
-//   ReactDOM.render(<Router>{routes}</Router>, document.getElementById('content'));
-// });
+var routes = (
+  <Route path="/users/:userId/polls">
+    <IndexRoute component={PollIndexPage}/>
+    <Route path="/users/:userId/polls/new" component={MultiPollForm}></Route>
+    <Route path="/users/:userId/polls/:pollId/edit" component={PollEdit}></Route>
+  </Route>
+);
+
+document.addEventListener('DOMContentLoaded', function(){
+  ReactDOM.render(<Router>{routes}</Router>, document.getElementById('content'));
+});
